2755ccce12f7f7e682051c30951d4c34d91161ee
geoom <hi.geoom@gmail.com>
2015-12-19 08:59:19 -0500

    add logo, update sitemap

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -3,6 +3,6 @@ class SitemapController < ApplicationController
 
 	def index
 		@static_pages = [home_url]
-		@products = Product.order("created_at DESC")
+		# @products = Product.order('created_at DESC')
 	end
 end
