8903d82d4b2a62814ced5d3fe9e0c8e70a412311
geoom <hi.geoom@gmail.com>
2015-12-27 17:26:40 -0500

    update sitemap

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -2,7 +2,7 @@ class SitemapController < ApplicationController
 	respond_to :xml
 
 	def index
-		@static_pages = [home_url]
+		@static_pages = [home_url, about_url, privacy_url, tos_url, new_request_url]
 		# @products = Product.order('created_at DESC')
 	end
 end
