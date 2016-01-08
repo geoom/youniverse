0f77131284a3a84534929e6ee3157a40f8062bcf
geoom <hi.geoom@gmail.com>
2016-01-07 20:01:37 -0500

    update home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,14 +3,6 @@ class StaticPagesController < ApplicationController
 
 		@products = Product.where(published: true).order(created_at: :desc)
 
-		# person1 = { :name => "Joe", :age => 35, :job => "plumber" }
-		#
-		# person2 = { :name => "Martin", :age => 35, :job => "plumber" }
-		#
-		# person3 = { :name => "George", :age => 35, :job => "plumber" }
-		#
-		# @persons = [person1, person2, person3]
-
 	end
 
 	def about
